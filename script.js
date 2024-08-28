bebd9aafa73fa3878f6bacdcde0df84ef782ed4b
ulugbekbrat <168886327+ulugbekbrat@users.noreply.github.com>
2024-08-28 10:18:52 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+let numbers = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,,17,18,19,20]
+
+let a = []
+
+
+for(i = 0; i < numbers.length; i++ ){
+
+    if(numbers[i] % 2 === 0){
+        numbers.push('a')
+        console.log(numbers[i]);
+        
+    }else{
+
+    }
+
+
+}   
